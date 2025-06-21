1a67ce8ad286a7945600947a6637a973fe40011c
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2025-06-21 09:58:38 -0600

    Initial Commit

---
+++ DailyProductivity/DailyProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyProductivityApp.swift
+//  DailyProductivity
+//
+//  Created by Rodrigo Carballo on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
